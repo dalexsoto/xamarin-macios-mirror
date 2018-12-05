050a4175b5f4ddac35d4677736104e0a8e59d228
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-12-05 16:59:07 +0100

    [maccore-issue-643] Fix build.

--- tests/xharness/Harness.cs
+++ tests/xharness/Harness.cs
@@ -242,22 +242,22 @@ namespace xharness
 			
 			// MacTestProjects.Add (new MacTestProject (Path.GetFullPath (Path.Combine (RootDirectory, "introspection", "Mac", "introspection-mac.csproj")), targetFrameworkFlavor: MacFlavors.Modern) { Name = "introspection" });
 
-			var hard_coded_test_suites = new [] {
+			// var hard_coded_test_suites = new [] {
 				// new { Directory = "mmptest", ProjectFile = "mmptest", Name = "mmptest", IsNUnit = true, Configurations = (string[]) null, Platform = "x86", },
 				// new { Directory = "msbuild-mac", ProjectFile = "msbuild-mac", Name = "MSBuild tests", IsNUnit = true, Configurations = (string[]) null, Platform = "x86" },
 				// new { Directory = "xammac_tests", ProjectFile = "xammac_tests", Name = "xammac tests", IsNUnit = false, Configurations = new string [] { "Debug", "Release" }, Platform = "AnyCPU" },
 				// new { Directory = "linker/mac/link all", ProjectFile = "link all-mac", Name = "link all", IsNUnit = false, Configurations = new string [] { "Debug", "Release" }, Platform = "x86", },
 				// new { Directory = "linker/mac/link sdk", ProjectFile = "link sdk-mac", Name = "link sdk", IsNUnit = false, Configurations = new string [] { "Debug", "Release" }, Platform = "x86", },
-			};
-			foreach (var p in hard_coded_test_suites) {
-				MacTestProjects.Add (new MacTestProject (Path.GetFullPath (Path.Combine (RootDirectory, p.Directory + "/" + p.ProjectFile + ".csproj")), generateVariations: false) {
-					Name = p.Name,
-					IsNUnitProject = p.IsNUnit,
-					SolutionPath = Path.GetFullPath (Path.Combine (RootDirectory, "tests-mac.sln")),
-					Configurations = p.Configurations,
-					Platform = p.Platform,
-				});
-			}
+			// };
+			// foreach (var p in hard_coded_test_suites) {
+			// 	MacTestProjects.Add (new MacTestProject (Path.GetFullPath (Path.Combine (RootDirectory, p.Directory + "/" + p.ProjectFile + ".csproj")), generateVariations: false) {
+			// 		Name = p.Name,
+			// 		IsNUnitProject = p.IsNUnit,
+			// 		SolutionPath = Path.GetFullPath (Path.Combine (RootDirectory, "tests-mac.sln")),
+			// 		Configurations = p.Configurations,
+			// 		Platform = p.Platform,
+			// 	});
+			// }
 
 			var bcl_suites = new string[] {
 				"mscorlib",
