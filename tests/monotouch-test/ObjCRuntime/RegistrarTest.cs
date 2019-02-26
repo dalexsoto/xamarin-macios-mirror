rolfbjarne/xamarin-macios

[monotouch-test] Adjust for arm64_32 abi.

tests/monotouch-test/ObjCRuntime/RegistrarTest.cs
@@ -1383,7 +1383,7 @@ namespace MonoTouchFixtures.ObjCRuntime {
 #if MONOMAC
 			var boolEncoding = "c";
 #else
-			var boolEncoding = (IntPtr.Size == 8 || TrampolineTest.IsArmv7k) ? "B" : "c";
+			var boolEncoding = (IntPtr.Size == 8 || TrampolineTest.IsArmv7k || TrampolineTest.IsArm64_32) ? "B" : "c";
 
 #endif
 			var exp = new string [] { "@", ":", "^v", "C", "c", "s", "s", "S", "i", "I", "q", "Q", "f", "d", boolEncoding, "@", ":", "#" };

dalexsoto/xamarin-macios-mirror

tests/monotouch-test/ObjCRuntime/RegistrarTest.cs
@@ -1383,7 +1383,7 @@ namespace MonoTouchFixtures.ObjCRuntime {
 #if MONOMAC
 			var boolEncoding = "c";
 #else
-			var boolEncoding = (IntPtr.Size == 8 || TrampolineTest.IsArmv7k) ? "B" : "c";
+			var boolEncoding = (IntPtr.Size == 8 || TrampolineTest.IsArmv7k || TrampolineTest.IsArm64CallingConvention) ? "B" : "c";
 
 #endif
 			var exp = new string [] { "@", ":", "^v", "C", "c", "s", "s", "S", "i", "I", "q", "Q", "f", "d", boolEncoding, "@", ":", "#" };